61823270afd2855d1afff17ffc48fe182e2ccab3
Y.A.E.N.N.E <yannick.schoenhaar@edu.tbz.ch>
2023-06-06 15:54:47 +0200

    more methods

--- express2/main.js
+++ express2/main.js
@@ -5,18 +5,28 @@ const fs = require('fs');
 const app = express();
 const port = 3000;
 
-app.get("/now", (response, request) => {
-    let time = new Date().toLocaleTimeString();
-    const timeZone = 'America/New_York';
+app.get('/now', (req, res) => {
+    let time = new Date().toLocaleTimeString('de-CH', { timeZone: req.query.q });
+    res.send(time);
+});
 
-    response.send(time)
+app.post('/name', (req, res) => {
+    const { firstName, lastName } = req.query;
+    const name = createName(firstName, lastName);
+    res.send(name);
+});
+
+app.get("/name", (req, res, name) => {
+    res.send(name)
 })
 
-app.post("/name", (response, request) => {
-    
-})
+function createName(firstName, lastName) {
+    // Logic to create a name using the provided firstName and lastName
+    // Example: return firstName + ' ' + lastName;
+    return `${firstName} ${lastName}`;
+}
 
-
+createName("Daniel", "Boulter")
 
 
 
