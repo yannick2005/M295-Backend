a2e459ffcf7f6b368326936799a17e0ead05a32e
Y.A.E.N.N.E <yannick.schoenhaar@edu.tbz.ch>
2023-06-06 12:56:43 +0200

    get methods

--- Express/main.js
+++ Express/main.js
@@ -6,6 +6,36 @@ app.get("/", (request, response) => {
     response.send("Hello World!")
 })
 
+// display current time
+app.get("/now", (request, response) => {
+    const time = new Date().toLocaleString("de-CH");
+    response.send(time);
+})
+
+// redirect to website of ZLI
+app.get("/zli", (request, response) =>{
+    response.redirect("https://www.zli.ch")
+})
+
+// Picks random name
+app.get("/name", (request, response) => {
+    let names = [
+        {name: "Daniel"}, {name: "Yannick"}, {name: "Matas"}, {name: "Michi"}, {name: "Theodor"}, 
+        {name: "Ruben"}, {name: "Robin"}, {name: "Giuseppe"}, {name: "Liam"}, {name: "Leon"}, 
+        {name: "Sven"}, {name: "Manuel"}, {name: "Marcel"}, {name: "Irene"}, {name: "Sakin"},
+        {name: "Zubair"}, {name: "Robert"}, {name: "Alexander"}, {name: "Simon"}, {name: "Leonardo"},
+    ]
+
+    const random = Math.floor(Math.random() * names.length);
+    response.send(names[random]);
+})
+
+// html converted text of the server
+app.get("/html", (request, response) => {
+    
+})
+
+// listens to port 3000
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
