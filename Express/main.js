22a44febeebed99d84d649009e6388e08e3003e8
Y.A.E.N.N.E <yannick.schoenhaar@edu.tbz.ch>
2023-06-06 14:31:45 +0200

    more get methods

--- Express/main.js
+++ Express/main.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const fs = require('fs')
 const app = express()
 const port = 3000
 
+// displays hello world!
 app.get("/", (request, response) => {
     response.send("Hello World!")
 })
@@ -19,20 +21,60 @@ app.get("/zli", (request, response) =>{
 
 // Picks random name
 app.get("/name", (request, response) => {
-    let names = [
-        {name: "Daniel"}, {name: "Yannick"}, {name: "Matas"}, {name: "Michi"}, {name: "Theodor"}, 
-        {name: "Ruben"}, {name: "Robin"}, {name: "Giuseppe"}, {name: "Liam"}, {name: "Leon"}, 
-        {name: "Sven"}, {name: "Manuel"}, {name: "Marcel"}, {name: "Irene"}, {name: "Sakin"},
-        {name: "Zubair"}, {name: "Robert"}, {name: "Alexander"}, {name: "Simon"}, {name: "Leonardo"},
+    const names = [
+        "Daniel", "Yannick", "Matas", "Michi", "Theodor",
+        "Ruben", "Robin", "Giuseppe", "Liam", "Leon", 
+        "Sven", "Manuel", "Marcel", "Irene", "Sakin", 
+        "Zubair", "Robert", "Alexander", "Simon", "Leonardo"
     ]
 
-    const random = Math.floor(Math.random() * names.length);
-    response.send(names[random]);
+    const random = names[Math.floor(Math.random() * names.length)];
+    response.send(random);
 })
 
 // html converted text of the server
 app.get("/html", (request, response) => {
-    
+    response.sendFile(`${__dirname}/index.html`)
+})
+
+app.get("/image", (request, response) => {
+    response.sendFile(`${__dirname}/img.jpg`)
+})
+
+app.get("/teapot", (request, response) => {
+    response.sendStatus(418)
+})
+
+app.get("/user-agent", (request, response) => {
+    const userAgent = request.headers["user-agent"]
+
+    response.send(userAgent);
+    // response.json({userAgent: request.headers("user-agent")})    
+})
+
+app.get("/secret", (request, response) => {
+    response.sendStatus(401);
+})
+
+app.get("/xml", (request, response) => {
+    fs.readFile('static.xml', (err, data) => {
+        if(err){
+            response.sendStatus("500");
+        } else {
+            response.type("xml")
+            response.send(data)
+        }
+    })
+})
+
+app.get("/me", (request, response) => {
+    const me = {
+        firstName: "Yannick",
+        lsatName: "Schönhaar",
+        age: 17,
+        place: "Kloten"
+    }
+    response.json(me)
 })
 
 // listens to port 3000
