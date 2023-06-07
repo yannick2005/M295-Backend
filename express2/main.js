c201ef7b8bd0a2c21df0c407216bf9c5abc1318b
Y.A.E.N.N.E <yannick.schoenhaar@edu.tbz.ch>
2023-06-07 09:52:02 +0200

    even more methods

--- express2/main.js
+++ express2/main.js
@@ -7,30 +7,85 @@ const port = 3000;
 
 const FormHelper = bodyParser.urlencoded();
 
-let createdName = ''; // Variable to store the created name
-
 app.get('/now', (req, res) => {
-    let time = new Date().toLocaleTimeString('de-CH', { timeZone: req.query.q });
-    res.send(time);
+    const timeZone = req.query.tz
+    const now = new Date().toLocaleTimeString('de-CH', {timeZone: timeZone})
+    res.send("Your current time is: " + now)
 });
 
-app.post('/name', (req, res) => {
-    const { firstName, lastName } = req.query;
-    createdName = createName(firstName, lastName); // Update the createdName variable
-    res.send(createdName);
+// app.get("/name", (req, res) => {
+//     res.sendFile(__dirname + "/form.html")
+// })
+
+// app.post("/name", FormHelper,(req, res) => {
+//     const name = req.body.name
+//     res.send(`Hello ${name}, you are a Michi!`)
+// })
+
+const names = new Set["Yannick", "Daniel", "Raphael", "Michael"]
+
+app.get("/names", (req, res) => {
+    res.send(res.json(Array.from(names)))
+})
+
+app.post("/names", FormHelper, (req, res) => {
+    const name = req.body.name
+    name.push()
+    res.sendStatus(201)
+})
+
+app.delete("/names", FormHelper,(req, res) => {
+    const name = req.body.name
+    if(names.has(name)){
+        names.delete()
+        res.sendStatus(204)
+    } else {
+        res.sendStatus(404)
+    }
+})
+
+app.get("/secret2", (req, res) => {
+    const auth = req.headers.auth
+    if(auth === "Basic aGFja2VyOjEyMzQ="){
+        res.sendStatus(200)
+    } else {
+        res.sendStatus(401)
+    }
 });
 
-app.get('/name', (req, res) => {
-    res.send(createdName); // Send the stored createdName
+app.get("/chuck", (req, res) => {
+    async function getJoke() {
+        const jokeResponse = await fetch("https://api.chucknorris.io/jokes/random");
+        const joke = await jokeResponse.json();
+        res.send(joke.value);
+    }
+    getJoke();
 });
 
-function createName(firstName, lastName) {
-    // Logic to create a name using the provided firstName and lastName
-    // Example: return firstName + ' ' + lastName;
-    return `${firstName} ${lastName}`;
-}
+app.get("/me", (req, res) => {
+    const me = {
+        name: "Yannick",
+        surname: "Schönhaar",
+        age: 17,
+        location: "Kloten",
+        street: "Bienenweg",
+        number: "11d"
+    };
+    res.send(me);
+});
 
-createName('Daniel', 'Boulter');
+app.patch("/me", (req, res) => {
+    const authHeader = req.header('Authorization');
+    if (authHeader === authHeaderMessage) {
+        const updatedMe = {
+            name: "Irene",
+            surname: "Schönhaar"
+        };
+        res.send(updatedMe);
+    } else {
+        res.status(401).send('Authentication error');
+    }
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
